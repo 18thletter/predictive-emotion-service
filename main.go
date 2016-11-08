b42138c9f1befb122f2bc45ad315c573cb2eb9eb
18thletter <raymund.lew@gmail.com>
2016-11-08 12:43:05 -0800

    Stub out the endpoint returns

--- main.go
+++ main.go
@@ -24,9 +24,55 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
+	// Serve the docs page
 	router.GET("/docs", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "docs.tmpl.html", nil)
 	})
 
+	// Group v1 API resources
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/datasets", GetAllDatasets)
+		v1.POST("/datasets", CreateDataset)
+		v1.POST("/datasets/:dataset_id", GetDataset)
+		v1.POST("/heartbeats", CreateHeartbeat)
+		v1.POST("/emotions", CreateEmotion)
+		v1.GET("/emotions", GetAllEmotions)
+		v1.POST("/correct", CorrectDataset)
+		v1.GET("/predict", GetPrediction)
+	}
+
 	router.Run(":" + port)
+}
+
+func GetAllDatasets(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func CreateDataset(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func GetDataset(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func CreateHeartbeat(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func CreateEmotion(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func GetAllEmotions(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func CorrectDataset(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
+}
+
+func GetPrediction(c *gin.Context) {
+	c.JSON(http.StatusOK, "{}")
 }
