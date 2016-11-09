6532d24b0ad08336b0d1a66f237de364a60f6982
18thletter <raymund.lew@gmail.com>
2016-11-08 16:31:02 -0800

    Add some migrations

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 func main() {
 	port := os.Getenv("PORT")
@@ -25,6 +30,9 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
+	// Database migrations (heroku)
+	router.POST("/migrate", migrateFunc)
+
 	// Group v1 API resources
 	v1 := router.Group("/v1")
 	{
@@ -39,6 +47,74 @@ func main() {
 	}
 
 	router.Run(":" + port)
+}
+
+func migrateFunc(c *gin.Context) {
+	if _, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS datasets (
+				id int SERIAL PRIMARY KEY,
+				created_at timestamp,
+				updated_at timestamp,
+				emotion_id REFERENCES emotions (id)
+			)
+		`); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error creating database table: %q", err))
+		return
+	}
+
+	if _, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS heartbeats (
+				id int SERIAL PRIMARY KEY,
+				start timestamp,
+				end timestamp
+			)
+		`); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error creating database table: %q", err))
+		return
+	}
+
+	if _, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS emotions (
+				id int SERIAL PRIMARY KEY,
+				emotion character varying(255)
+			)
+		`); err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error creating database table: %q", err))
+		return
+	}
+
+	// if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+	// 	c.String(http.StatusInternalServerError,
+	// 		fmt.Sprintf("Error creating database table: %q", err))
+	// 	return
+	// }
+	//
+	// if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+	// 	c.String(http.StatusInternalServerError,
+	// 		fmt.Sprintf("Error incrementing tick: %q", err))
+	// 	return
+	// }
+	//
+	// rows, err := db.Query("SELECT tick FROM ticks")
+	// if err != nil {
+	// 	c.String(http.StatusInternalServerError,
+	// 		fmt.Sprintf("Error reading ticks: %q", err))
+	// 	return
+	// }
+	//
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var tick time.Time
+	// 	if err := rows.Scan(&tick); err != nil {
+	// 		c.String(http.StatusInternalServerError,
+	// 			fmt.Sprintf("Error scanning ticks: %q", err))
+	// 		return
+	// 	}
+	// 	c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+	// }
 }
 
 func GetAllDatasets(c *gin.Context) {
